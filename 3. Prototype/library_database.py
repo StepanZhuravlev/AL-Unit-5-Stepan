ff999fed3777817602be42e1fe860a1acad8ece4
StepanZhuravlev <65539533+StepanZhuravlev@users.noreply.github.com>
2021-12-02 10:47:25 +0000

    02/12/2021 - Period 2

--- 3. Prototype/library_database.py
+++ 3. Prototype/library_database.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-
+# BOOKS TABLE:
 def create_book_table():
     """Creates a table for storing book details"""
     library_database = sqlite3.connect("Library.db")
@@ -38,9 +38,49 @@ def insert_book_data(new_book_data):
                                                   new_book_data[8], new_book_data[9], new_book_data[10], new_book_data[11],
                                                   new_book_data[12], new_book_data[13], new_book_data[14], new_book_data[15])
                                  )
-    print("New data inserted successfully")
+    print("New book data inserted successfully")
     library_database.commit()
     library_database.close()
 
 
-create_book_table()
+# MEMBERS TABLE:
+def create_members_table():
+    """Creates a table for storing member details"""
+    library_database = sqlite3.connect("Library.db")
+    members_table_cursor = library_database.cursor()
+    members_table_cursor.execute("""CREATE TABLE IF NOT EXISTS Members(
+                                    MemberID VARCHAR(10) PRIMARY KEY,
+                                    FirstName VARCHAR(50),
+                                    LastName VARCHAR(50),
+                                    DateOfBirth DATE,
+                                    Email VARCHAR(50),
+                                    SchoolYear INTEGER,
+                                    MemberType VARCHAR(10))
+                                    """)
+    library_database.commit()
+    library_database.close()
+    print("members table created")
+    
+    
+def insert_member_data(new_member_data):
+    """Adds the details entered by the user to Members table in Library.db database"""
+    library_database = sqlite3.connect("Library.db")
+    members_table_cursor = library_database.cursor()
+    members_table_cursor.execute("""INSERT INTO Members(MemberID, FirstName, LastName, DateOfBirth, Email, SchoolYear, MemberType)
+    VALUES (?,?,?,?,?,?,?)""", (new_member_data[0], new_member_data[1], new_member_data[2], new_member_data[3],
+                                new_member_data[4], new_member_data[5], new_member_data[6])
+                               )
+    print("New member data inserted successfully")
+    library_database.commit()
+    library_database.close()
+    
+
+#create_book_table()
+
+SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';
+
+create_members_table()
+
+
+new_member_data = ["MemID2", "Stepan", "Zhuravlev", "01/01/2000", "szhuravlev308@ccblearning.org.uk", "14", "Student"]
+insert_member_data(new_member_data)
