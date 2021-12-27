020e085b06855389a3053a890c1c00ba89aa4094
StepanZhuravlev <65539533+StepanZhuravlev@users.noreply.github.com>
2021-12-27 11:26:14 +0700

    Delete back_to_menu.py

--- 3. Prototype/back_to_menu.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-
-
-def close_current_open_new(current_window, new_window):
-    """Closes <current_window> and opens <new_window>"""
-    new_window = Toplevel(current_window)
-    new_window.mainloop()
