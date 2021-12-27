4d891dfd43ae5c50eea56fd908559e160354b7e4
StepanZhuravlev <65539533+StepanZhuravlev@users.noreply.github.com>
2021-12-27 17:26:11 +0700

    Update switch_windows.py

--- 3. Prototype/switch_windows.py
+++ 3. Prototype/switch_windows.py
@@ -9,7 +9,8 @@ def back_to_menu(current, menu):
 
 
 def closing_using_x(current, menu):
-    """Asks the user if they want to quit, closes all windows if they do"""
+    """Runs when the user tries to close a window using the "X" button.
+    Asks the user if they want to quit, closes all windows if they do"""
     if messagebox.askokcancel("", "Do you want to quit?"):  # the windows get closed if askokcancel() evaluates to True
         current.destroy()
         menu.destroy()
