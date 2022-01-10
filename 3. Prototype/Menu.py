583f2a138115ee12e214f5918120771ba83e87a2
StepanZhuravlev <65539533+StepanZhuravlev@users.noreply.github.com>
2022-01-10 12:02:16 +0000

    10/01/2022 - Period 3

--- 3. Prototype/Menu.py
+++ 3. Prototype/Menu.py
@@ -4,7 +4,7 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
-from library_database import insert_book_data, insert_member_data
+from library_database import insert_book_data, insert_member_data, insert_loan_data
 from get_table_records import display_table_records
 from switch_windows import back_to_menu, closing_using_x
 
