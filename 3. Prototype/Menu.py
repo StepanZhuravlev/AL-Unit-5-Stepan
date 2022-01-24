c9a5b354051bcc99798db4887deafd615c2e675c
StepanZhuravlev <65539533+StepanZhuravlev@users.noreply.github.com>
2022-01-24 09:57:49 +0000

    24/01/2022 -Period 1

--- 3. Prototype/Menu.py
+++ 3. Prototype/Menu.py
@@ -224,73 +224,31 @@ def open_add_member_window():
     
     
 def open_add_loan_window():
-    """"""
-
-    def widget_selection(var):
-        #if var.get() == "ID":
-            # display find_id_lbl, find_id_ent
-            
-        #if var.get() == "Name":
-            # display find_first_name_lbl, find_last_name_lbl, find_first_name_ent, find_last_name_ent
-        pass
-
     menu_window.withdraw()
     find_member_window = Tk()
     find_member_window.title("Find a member")
 
-    # Let the user enter member details (e.g. ID or first name + last name)
-    # If a matching record is found in the database, create new record in Loans with the details of student added to it.
-    # LoanID - generate automatically on record creation
-    # LoanDate - Today / Enter value
-    # LoanDuration - Default (how many days?) / Enter value
-    # DueForReturn - Calculate using LoanDate and LoanDuration
-    # IsDamaged, IsLost - not known at the moment of record creation, need a separate form for adding those values
-    # ISBN (FK) - take from the chosen book record
-    # MemberID (FK) - take from the matching database record
-    # User inputs: LoanDate, LoanDuration, Find book by name/isbn/author
+    def selection():
+        selected = id_or_name_var.get()
+        id_or_name_lbl.config(text=selected)
 
-    # grid_remove() - keeps row and column args, widget can be put back by using .grid() (with no args)
-    # grid_forget() - forgets row and column args
-    # https://xiith.com/python-tkinter-program-for-radiobutton-with-an-event-handling/
-
-    # value = "You selected the option " + str(self.radio.get())
-    # self.var.set(value)
+    # StringVar used to store the result of selection:
+    id_or_name_var = StringVar()
 
     # labels - instantiation
-    find_member_lbl = Label(find_member_window, text="Find a member using: ")
-    find_id_lbl = Label(find_member_window, text="Member ID: ")
-    find_first_name_lbl = Label(find_member_window, text="First name: ")
-    find_last_name_lbl = Label(find_member_window, text="Last name: ")
-
-    # entry fields and StringVars - instantiation
-    find_id_ent = Entry(find_member_window)  # make visible when find_member_id_rbtn is selected
-    find_member_var = StringVar()
-    find_first_name_ent = Entry(find_member_window)  # make visible when find_member_name is selected
-    find_last_name_ent = Entry(find_member_window)  # make visible when find_member_name is selected
-
-
-    # buttons - instantiation
-    find_member_id_rbtn = Radiobutton(find_member_window, text="ID", variable=find_member_var, value="ID")
-    find_member_name_rbtn = Radiobutton(find_member_window, text="First name and last name", variable=find_member_var, value="Name")
-    find_member_btn = Button(find_member_window, text="Find a member")  # command = search algorithm
-    back_to_menu_btn = Button(find_member_window, text="Back to Menu", command=lambda: back_to_menu(find_member_window, menu_window))
+    id_or_name_lbl = Label(find_member_window, text="")
 
     # labels - geometry
-    find_member_lbl.grid(row=0, column=0, padx=5, pady=5)
-    find_id_lbl.grid(row=1, column=0, padx=5, pady=5)
-    find_first_name_lbl.grid(row=1, column=1, padx=5, pady=5)
-    find_last_name_lbl.grid(row=2, column=1, padx=5, pady=5)
+    id_or_name_lbl.grid(row=1, column=1, padx=5, pady=5)
 
-    # entry fields and StringVars - geometry
+    # radiobuttons - instantiation
+    find_member_id_rbtn = Radiobutton(find_member_window, text="ID", variable=id_or_name_var, value="ID", command=selection)
+    find_member_name_rbtn = Radiobutton(find_member_window, text="First name and last name", variable=id_or_name_var, value="Name", command=selection)
 
-
-    # buttons - geometry
+    # radiobuttons - geometry
     find_member_id_rbtn.grid(row=0, column=1, padx=5, pady=5)
     find_member_name_rbtn.grid(row=0, column=2, padx=5, pady=5)
-    find_member_btn.grid(row=3, column=0, padx=5, pady=5)
-    back_to_menu_btn.grid(row=3, column=1, padx=5, pady=5)
 
-    
     find_member_window.protocol("WM_DELETE_WINDOW", lambda: closing_using_x(find_member_window, menu_window))  # imported from switch_windows.py
     find_member_window.mainloop()
     
--- 3. Prototype/old_get_loan_data_code.py
+++ 3. Prototype/old_get_loan_data_code.py
@@ -1,67 +1,70 @@
-def get_loan_data():
-        list_of_ent_fields = [loan_id_ent, loan_date_ent, loan_duration_ent, due_for_return_ent, is_damaged_ent, is_lost_ent, isbn_ent, member_id_ent]  # add names of stringvars
-        get_form_data_func(list_of_ent_fields, insert_loan_data)  # imported from get_form_data.py
+def open_add_loan_window():
+    """"""
+
+    def widget_selection(var):
+        # if var.get() == "ID":
+        # display find_id_lbl, find_id_ent
+
+        # if var.get() == "Name":
+        # display find_first_name_lbl, find_last_name_lbl, find_first_name_ent, find_last_name_ent
+        pass
 
     menu_window.withdraw()
-    add_loan_window = Tk()
-    add_loan_window.title("Loan data capture window")
-    
-    # add_loan_window - labels - instantiation
-    loan_id_lbl = Label(add_loan_window, text="Loan ID:")
-    loan_date_lbl = Label(add_loan_window, text="Loan date:")
-    loan_duration_lbl = Label(add_loan_window, text="Loan duration:")
-    due_for_return_lbl = Label(add_loan_window, text="Due for return:")
-    is_damaged_lbl = Label(add_loan_window, text="Is damaged:")
-    is_lost_lbl = Label(add_loan_window, text="Is lost:")
-    isbn_lbl = Label(add_loan_window, text="ISBN:")
-    member_id_lbl = Label(add_loan_window, text="Member ID:")
-    
-    # add_loan_window - labels - geometry
-    loan_id_lbl.grid(row=0, column=0, padx=5, pady=5)
-    loan_date_lbl.grid(row=1, column=0, padx=5, pady=5)
-    loan_duration_lbl.grid(row=2, column=0, padx=5, pady=5)
-    due_for_return_lbl.grid(row=3, column=0, padx=5, pady=5)
-    is_damaged_lbl.grid(row=4, column=0, padx=5, pady=5)
-    is_lost_lbl.grid(row=5, column=0, padx=5, pady=5)
-    isbn_lbl.grid(row=6, column=0, padx=5, pady=5)
-    member_id_lbl.grid(row=7, column=0, padx=5, pady=5)
-    
-    # add_loan_window - entry fields - instantiation and StringVars
-    loan_id_ent_var = StringVar()
-    loan_id_ent = Entry(add_loan_window, textvariable=loan_id_ent_var)
-    loan_date_ent_var = StringVar()
-    loan_date_ent = Entry(add_loan_window, textvariable=loan_date_ent_var)
-    loan_duration_ent_var = StringVar()
-    loan_duration_ent = Entry(add_loan_window, textvariable=loan_duration_ent_var)
-    due_for_return_ent_var = StringVar()
-    due_for_return_ent = Entry(add_loan_window, textvariable=due_for_return_ent_var)
-    is_damaged_ent_var = StringVar()
-    is_damaged_ent = Entry(add_loan_window, textvariable=is_damaged_ent_var)
-    is_lost_ent_var = StringVar()
-    is_lost_ent = Entry(add_loan_window, textvariable=is_lost_ent_var)
-    isbn_ent_var = StringVar()
-    isbn_ent = Entry(add_loan_window, textvariable=isbn_ent_var)
-    member_id_ent_var = StringVar()
-    member_id_ent = Entry(add_loan_window, textvariable=member_id_ent_var)
-    
-    # add_loan_window - entry fields - geometry
-    loan_id_ent.grid(row=0, column=1, padx=5, pady=5)
-    loan_date_ent.grid(row=1, column=1, padx=5, pady=5)
-    loan_duration_ent.grid(row=2, column=1, padx=5, pady=5)
-    due_for_return_ent.grid(row=3, column=1, padx=5, pady=5)
-    is_damaged_ent.grid(row=4, column=1, padx=5, pady=5)
-    is_lost_ent.grid(row=5, column=1, padx=5, pady=5)
-    isbn_ent.grid(row=6, column=1, padx=5, pady=5)
-    member_id_ent.grid(row=7, column=1, padx=5, pady=5)
-    
-    # add_loan_window - buttons - instantiation
-    loan_insert_btn = Button(add_loan_window, text="Save to the database", command=get_loan_data)
-    back_to_menu_btn = Button(add_loan_window, text="Back to Menu", command=lambda: back_to_menu(add_loan_window, menu_window))  # imported from switch_windows.py
-    
-    
-    # add_loan_window - buttons - geometry
-    loan_insert_btn.grid(row=8, column=0, padx=5, pady=5)
-    back_to_menu_btn.grid(row=8, column=1, padx=5, pady=5)
-    
-    add_loan_window.protocol("WM_DELETE_WINDOW", lambda: closing_using_x(add_loan_window, menu_window))  # imported from switch_windows.py
-    add_loan_window.mainloop()
+    find_member_window = Tk()
+    find_member_window.title("Find a member")
+
+    # Let the user enter member details (e.g. ID or first name + last name)
+    # If a matching record is found in the database, create new record in Loans with the details of student added to it.
+    # LoanID - generate automatically on record creation
+    # LoanDate - Today / Enter value
+    # LoanDuration - Default (how many days?) / Enter value
+    # DueForReturn - Calculate using LoanDate and LoanDuration
+    # IsDamaged, IsLost - not known at the moment of record creation, need a separate form for adding those values
+    # ISBN (FK) - take from the chosen book record
+    # MemberID (FK) - take from the matching database record
+    # User inputs: LoanDate, LoanDuration, Find book by name/isbn/author
+
+    # grid_remove() - keeps row and column args, widget can be put back by using .grid() (with no args)
+    # grid_forget() - forgets row and column args
+    # https://xiith.com/python-tkinter-program-for-radiobutton-with-an-event-handling/
+
+    # value = "You selected the option " + str(self.radio.get())
+    # self.var.set(value)
+
+    # labels - instantiation
+    find_member_lbl = Label(find_member_window, text="Find a member using: ")
+    find_id_lbl = Label(find_member_window, text="Member ID: ")
+    find_first_name_lbl = Label(find_member_window, text="First name: ")
+    find_last_name_lbl = Label(find_member_window, text="Last name: ")
+
+    # entry fields and StringVars - instantiation
+    find_id_ent = Entry(find_member_window)  # make visible when find_member_id_rbtn is selected
+    find_member_var = StringVar()
+    find_first_name_ent = Entry(find_member_window)  # make visible when find_member_name is selected
+    find_last_name_ent = Entry(find_member_window)  # make visible when find_member_name is selected
+
+    # buttons - instantiation
+    find_member_id_rbtn = Radiobutton(find_member_window, text="ID", variable=find_member_var, value="ID")
+    find_member_name_rbtn = Radiobutton(find_member_window, text="First name and last name", variable=find_member_var,
+                                        value="Name")
+    find_member_btn = Button(find_member_window, text="Find a member")  # command = search algorithm
+    back_to_menu_btn = Button(find_member_window, text="Back to Menu",
+                              command=lambda: back_to_menu(find_member_window, menu_window))
+
+    # labels - geometry
+    find_member_lbl.grid(row=0, column=0, padx=5, pady=5)
+    find_id_lbl.grid(row=1, column=0, padx=5, pady=5)
+    find_first_name_lbl.grid(row=1, column=1, padx=5, pady=5)
+    find_last_name_lbl.grid(row=2, column=1, padx=5, pady=5)
+
+    # entry fields and StringVars - geometry
+
+    # buttons - geometry
+    find_member_id_rbtn.grid(row=0, column=1, padx=5, pady=5)
+    find_member_name_rbtn.grid(row=0, column=2, padx=5, pady=5)
+    find_member_btn.grid(row=3, column=0, padx=5, pady=5)
+    back_to_menu_btn.grid(row=3, column=1, padx=5, pady=5)
+
+    find_member_window.protocol("WM_DELETE_WINDOW", lambda: closing_using_x(find_member_window,
+                                                                            menu_window))  # imported from switch_windows.py
+    find_member_window.mainloop()
