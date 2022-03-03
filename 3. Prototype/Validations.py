3c49ec785782d7761a969453b41136d2de928263
StepanZhuravlev <65539533+StepanZhuravlev@users.noreply.github.com>
2022-03-03 21:26:14 +0000

    Update Validations.py

--- 3. Prototype/Validations.py
+++ 3. Prototype/Validations.py
@@ -1,42 +1,52 @@
-#presence(value), lookup(value), length(value, length), range(value, min, max), format(value, regex)
-
-
 def presence_check(value):
-    # Convert the value to string if it is of a different type
-    if not isinstance(value, str):
-        value_to_string = str(value)
-    if len(value_to_string) == 0:
-        # Code to be executed is value is absent
-        flag = False
-        return flag
+    # len function doesn't work with numeric types -> convert int, float to str
+    if isinstance(value, int) or isinstance(value, float):
+        value = str(value)
+    if len(value) == 0:
+        return False
     else:
-        # Code to be executed if value is present
-        flag = True
-        return flag
+        return True
 
 
 def lookup(value, appropriate_values):
-    # appropriate_values is a list
-    # In appropriate_values, every letter should be capital
     if value in appropriate_values:
-        # Code to be executed if there is a match
-        flag = True
-        return flag
+        return True
     else:
-        # Code to be executed if the value is inappropriate
-        flag = False
-        return flag
+        return False
         
         
-def length_check(value, length):
-    # If value is str/list/tuple/dictionary, leave as is
-    # If value is of a different type, convert to str
-    pass
+def length_check(value, max_length):
+    if not isinstance(value, tuple) or not isinstance(value, list) or not isinstance(value, dict):  # if variable is not tuple, list, or dictionary, convert to string
+        value = str(value)
+
+    if len(value) > max_length:  # if longer than length (length check fail), return False
+        return False
+    else:  # else (length check pass), return True
+        return True
 
 
 def format_check(value, regex):
     pass
 
 
-def range_check(value, min, max):
-    pass
+def range_check(value, min_value, max_value):
+    """Checks if an integer's value is within the range"""
+    if value in range(min_value, max_value+1):
+        return False
+    else:
+        return True
+
+# Type checks needed for: string, int, float, boolean, currency (float to 2 d.p.)
+# Format check needed for date:
+    # 00 slash 00 slash 0000, where 0 is a digit
+    # first digit in dd can only be 0, 1, 2, 3 (if month != 02
+    # second
+
+
+sample = {"a":1, "b":2}
+print(str(sample))
+print(len(sample))
+print(type(sample))
+print(isinstance(sample, dict))
+
+print(9 in range(0, 9+1))
