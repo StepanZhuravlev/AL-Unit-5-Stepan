1aa4617dd419c8a8e578f98593edc86e9cb12858
StepanZhuravlev <65539533+StepanZhuravlev@users.noreply.github.com>
2022-03-07 07:48:21 +0000

    Update Validations.py

--- 3. Prototype/Validations.py
+++ 3. Prototype/Validations.py
@@ -1,3 +1,6 @@
+import re
+
+
 def presence_check(value):
     # len function doesn't work with numeric types -> convert int, float to str
     if isinstance(value, int) or isinstance(value, float):
@@ -36,11 +39,65 @@ def range_check(value, min_value, max_value):
     else:
         return True
 
+
+def type_check_int(value):
+    if isinstance(value, int):
+        return True
+    else:
+        return False
+
+
+def type_check_float(value):
+    if isinstance(value, float):
+        return True
+    else:
+        return False
+
+
+def type_check_string(value):
+    if isinstance(value, string):
+        return True
+    else:
+        return False
+
+
+def type_check_boolean(value):
+    if isinstance(value, bool):
+        return True
+    else:
+        return False
+
+
+def type_check_currency(value):  # float to 2 d.p.
+    if isinstance(value, float):
+        # check 2 d.p.
+        pass
+    else:
+        return False
+
 # Type checks needed for: string, int, float, boolean, currency (float to 2 d.p.)
 # Format check needed for date:
-    # 00 slash 00 slash 0000, where 0 is a digit
-    # first digit in dd can only be 0, 1, 2, 3 (if month != 02
-    # second
+    # dd slash mm slash yyyy
+    # first digit in dd can only be 0, 1, 2, 3 (if month != 02)
+    # second digit in dd can be any
+    # first digit in mm is 0 or 1
+    # second digit is any
+    # any digits in yyyy
+
+
+def type_check_date(value):
+    date_pattern = r"[0-3][0-9][/][0-1][0-9][/][1-2][0-9][0-9][0-9]"
+    valid_dd_pattern = [0, 1]
+    if re.match(value, date_pattern):
+        # proceed to further checks
+        if value[0] == "3":  # if day is 30 or 31
+            if value[1] not in valid_dd_pattern:
+                return False
+
+        pass
+    else:
+        return False
+
 
 
 sample = {"a":1, "b":2}
@@ -48,5 +105,7 @@ print(str(sample))
 print(len(sample))
 print(type(sample))
 print(isinstance(sample, dict))
+print(type(1))
+print(type(True))
 
 print(9 in range(0, 9+1))
